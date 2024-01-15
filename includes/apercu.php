776d9c4bfe0a4c30252a230c6b45844acf930a25
VanneSolo <lesolitaire.bigjay@gmail.com>
2024-01-15 20:22:57 +0100

    images qui fonctionnent

--- includes/apercu.php
+++ includes/apercu.php
@@ -1,10 +1,11 @@
 <?php
     if(isset($_GET['id'])){
+        
         $id = intval($_GET['id']);
         include_once('database.php');
-        $i = $db->prepare("SELECT img_id, img_type, img_blob FROM images WHERE img_id = ".$id);
+        $i = $db->query("SELECT img_id, img_type, img_blob FROM images WHERE img_id = ".$id);
         $i->execute();
-        $im = $i->fethcAll();
+        $im = $i->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT);
 
         if(!$im[0]){
             echo "ID d'image inconnu";
--- includes/display.php
+++ includes/display.php
@@ -1,15 +1,10 @@
 <?php
     include_once 'database.php';
-    $import = $db->prepare("SELECT img_nom,img_id FROM images ORDER BY img_nom");
+    $import = $db->prepare("SELECT img_nom,img_id,img_blob FROM images ORDER BY img_nom");
+    //$import->bindParam("i", $_GET['image_id']);
     $import->execute();
-    //$images = $import->fetchAll();
-
-    // foreach($images as $image){
-    //     echo "<a href=\"apercu.php?id=".$image[1]."\">".$image[0]."</a><br/>";
-    //     //var_dump($images);
-    // }
-
-    while($image = $import->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)){
-        echo "<a href=\"apercu.php?id=".$image[1]."\">".$image[0]."</a><br/>";
+    while ($row = $import->fetch(PDO::FETCH_ASSOC)){
+        echo $row['img_nom'];
+        echo '<img src = "data:image/png;base64,' . base64_encode($row['img_blob']) . '" width = "80px" height = "80px"/>';
     }
 ?>
